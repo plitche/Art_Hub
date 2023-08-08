33ca92a07b54862657b0c6f486f7ded47f479015
plitche <kysu728@naver.com>
2023-08-08 23:42:06 +0900

    db 연결

--- src/main/java/com/ArtHub/ArtHubApplication.java
+++ src/main/java/com/ArtHub/ArtHubApplication.java
@@ -2,8 +2,9 @@ package com.ArtHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ArtHubApplication {
 
 	public static void main(String[] args) {
