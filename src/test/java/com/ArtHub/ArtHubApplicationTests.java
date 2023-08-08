2c75e21e2907603f5ff6d22041fb2fadb502e64b
plitche <kysu728@naver.com>
2023-08-08 23:19:34 +0900

    create spring boot project

---
+++ src/test/java/com/ArtHub/ArtHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArtHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
