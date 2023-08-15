cbc3f9badd18b424849798ed78701b7e3e609dfc
plitche <kysu728@naver.com>
2023-08-15 20:55:44 +0900

    test 코드 작성(미완)

--- src/test/java/com/ArtHub/ArtHubApplicationTests.java
+++ src/test/java/com/ArtHub/ArtHubApplicationTests.java
@@ -1,13 +1,41 @@
-package com.ArtHub;
+package com.artHub;
 
+import com.artHub.sample.SampleService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Arrays;
+import java.util.List;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ArtHubApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private SampleService sampleService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void sampleTest() throws Exception {
+		List<String> expectedUsers = Arrays.asList("User1", "User2", "User3");
+
+		// 모의 데이터 반환 설정
+		when(sampleService.findUsers()).thenReturn(expectedUsers);
+
+		mockMvc.perform(get("/sample"))
+				.andExpect(status().isOk())
+				.andExpect(content().json("[\"User1\", \"User2\", \"User3\"]"));
+	}
 }
