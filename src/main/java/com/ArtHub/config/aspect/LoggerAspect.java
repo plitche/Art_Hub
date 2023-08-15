48f1818521970398e514d20485dc05164f64889c
plitche <kysu728@naver.com>
2023-08-15 20:10:06 +0900

    interceptor setting

---
+++ src/main/java/com/ArtHub/config/aspect/LoggerAspect.java
@@ -0,0 +1,11 @@
+package com.artHub.config.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+// @Aspect
+public class LoggerAspect {
+
+}
---
+++ src/main/java/com/ArtHub/config/interceptor/LoggerInterceptor.java
@@ -0,0 +1,30 @@
+package com.artHub.config.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (log.isDebugEnabled()) {
+            log.debug("======================================          START         ======================================");
+            log.debug(" Request URI \t:  " + request.getRequestURI());
+        }
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (log.isDebugEnabled()) {
+            log.debug("======================================           END          ======================================\n");
+        }
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+}
